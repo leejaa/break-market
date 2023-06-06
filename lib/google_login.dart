53ba07ec597f16c321ba975498636f120995f6fe
break-jahun <jh.lee@break.co.kr>
2023-06-06 17:34:56 +0900

    test

---
+++ lib/google_login.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:http/http.dart' as http;
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+class GoogleLogin {
+  late String? accessToken;
+  late String? refreshToken;
+
+  getAccessToken() {
+    return accessToken;
+  }
+
+  getRefreshToken() {
+    return refreshToken;
+  }
+
+  login() async {
+    GoogleSignIn googleSignIn = GoogleSignIn(scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ]);
+
+    GoogleSignInAccount? signInAccount = await googleSignIn.signIn();
+
+    GoogleSignInAuthentication credential = await signInAccount!.authentication;
+
+    var client = http.Client();
+    await client.post(Uri.http('172.30.1.22:3000', '/api/google_login'), body: {
+      // .post(Uri.http('192.168.0.93:3000', '/api/google_login'), body: {
+      // .post(Uri.https('break-webview.vercel.app', '/api/google_login'),
+      //     body: {
+      'email': signInAccount.email,
+    });
+
+    accessToken = credential.accessToken;
+    refreshToken = credential.idToken;
+  }
+}
--- lib/webview_manager.dart
+++ lib/webview_manager.dart
@@ -1,4 +1,5 @@
 import 'package:breakmarket/apple_login.dart';
+import 'package:breakmarket/google_login.dart';
 import 'package:breakmarket/kakao_login.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -9,6 +10,21 @@ import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 class WebViewManager {
   late final WebViewController controller;
   late final PlatformWebViewControllerCreationParams params;
+
+  addGoogleChannel() {
+    controller.addJavaScriptChannel('googlelogin',
+        onMessageReceived: (JavaScriptMessage message) async {
+      var googleLogin = GoogleLogin();
+      await googleLogin.login();
+
+      var accessToken = googleLogin.getAccessToken();
+      var refreshToken = googleLogin.getRefreshToken();
+
+      await controller.runJavaScriptReturningResult(
+        "setCookie('{\"accessToken\": \"${accessToken}\", \"refreshToken\": \"${refreshToken}\", \"loginType\": \"kakao\"}')",
+      );
+    });
+  }
 
   addKakaoChannel() {
     controller.addJavaScriptChannel('kakaologin',
@@ -53,6 +69,7 @@ class WebViewManager {
 
     addKakaoChannel();
     addAppleChannel();
+    addGoogleChannel();
 
     controller.setJavaScriptMode(JavaScriptMode.unrestricted);
 
@@ -66,8 +83,8 @@ class WebViewManager {
     }
 
     controller.setJavaScriptMode(JavaScriptMode.unrestricted);
-    controller.loadRequest(Uri.parse('https://break-webview.vercel.app'));
+    // controller.loadRequest(Uri.parse('https://break-webview.vercel.app'));
     // controller.loadRequest(Uri.parse('http://192.168.0.93:3000'));
-    // controller.loadRequest(Uri.parse('http://172.30.1.22:3000'));
+    controller.loadRequest(Uri.parse('http://172.30.1.22:3000'));
   }
 }
